f5a50c43690b10adf71693e0c03827be92f93bcd
Patrick Bucher <patrick.bucher@mailbox.org>
2022-06-29 16:49:30 +0200

    initial commit: de-duplicate messages in a given time-frame

---
+++ dedupe.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var messages = []string{"A", "B", "C"}
+
+type Message struct {
+	Timestamp time.Time
+	Content   string
+}
+
+func (m Message) String() string {
+	return fmt.Sprintf("[%v]: %s", m.Timestamp, m.Content)
+}
+
+func (m Message) Equal(other Message) bool {
+	return m.Content == other.Content
+}
+
+func produce(q chan<- Message, freq time.Duration) {
+	ticker := time.NewTicker(freq)
+	for {
+		<-ticker.C
+		i := rand.Intn(len(messages))
+		message := Message{Timestamp: time.Now(), Content: messages[i]}
+		q <- message
+		log.Println("produced", message)
+	}
+}
+
+func consume(q <-chan Message) {
+	for {
+		message := <-q
+		log.Println("consumed", message)
+	}
+}
+
+func dedupe(source <-chan Message, frame time.Duration) chan Message {
+	sink := make(chan Message)
+	ticker := time.NewTicker(frame)
+	go func(s chan Message, t *time.Ticker) {
+		buffer := make(map[string]Message, 0)
+		for {
+			select {
+			case <-t.C:
+				for _, m := range buffer {
+					sink <- m
+				}
+				buffer = make(map[string]Message, 0)
+			case m := <-source:
+				if _, ok := buffer[m.Content]; !ok {
+					buffer[m.Content] = m
+				} else {
+					log.Println("duplicate", m, "-> drop!")
+				}
+			}
+		}
+	}(sink, ticker)
+	return sink
+}
+
+func main() {
+	queue := make(chan Message)
+	deduped := dedupe(queue, time.Second*5)
+	go consume(deduped)
+	go produce(queue, time.Second)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/patrickbucher/dedupe
+
+go 1.18
